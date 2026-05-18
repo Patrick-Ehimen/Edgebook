50f96f9ece98a0c8c20d57e59f4339e670240fbd
0xOse <0xosepatrick@gmail.com>
2026-05-18 13:57:15 +0100

    fix(api): bundle ccxt inline, ignore optional proxy/protobuf deps via IgnorePlugin

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,13 +1,20 @@
 const nodeExternals = require('webpack-node-externals');
+const { IgnorePlugin } = require('webpack');
 
 module.exports = (options) => ({
   ...options,
-  // Bundle @edgebook/* workspace packages inline.
-  // ccxt is kept external to avoid bundling its optional deps (http-proxy-agent, etc.).
+  // Bundle @edgebook/* workspace packages and ccxt inline.
+  // ccxt is pure JS but requires optional native deps for proxy + dYdX support.
+  // We don't use those features, so IgnorePlugin silences the missing-module errors.
   externals: [
-    { ccxt: 'commonjs ccxt' },
     nodeExternals({
-      allowlist: [/^@edgebook\//],
+      allowlist: [/^@edgebook\//, /^ccxt/],
     }),
+  ],
+  plugins: [
+    ...(options.plugins ?? []),
+    new IgnorePlugin({ resourceRegExp: /^http-proxy-agent$/ }),
+    new IgnorePlugin({ resourceRegExp: /^socks-proxy-agent$/ }),
+    new IgnorePlugin({ resourceRegExp: /^protobufjs/ }),
   ],
 });
